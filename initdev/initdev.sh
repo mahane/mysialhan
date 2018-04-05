bcbf2c606bbbeb1fb15a117e249910c99f693431
mahane <iratni.asmahene@gmail.com>
2018-04-05 22:29:17 +0200

    mysialhan

--- initdev/initdev.sh
+++ initdev/initdev.sh
@@ -7,37 +7,39 @@ echo 1
 else
 if [ $1 == -help ] #on teste si l'utilisateur veut voir le help
 then echo "initdev :est une commande qui permet de créer un répertoire contenant 3 fichiers main,Makefile et LICENSE ."
-echo "Syntax : initdev nom_du_projet."
-echo "args : nom_du_projet :le nom de votre répertoire,il doit etre non vide."
+echo "Syntax : initdev nom_du_projet langage-du-projet "
+echo "args : nom_du_projet :le nom de votre répertoire,il doit etre non vide,langage-du-projet:c,c++,python,latex,beamer;type de licence:GPL,MIT"
 echo "author : IRATNI Asmahene , e-mail:iratni.asmahene@gmail.com" 
 else
 mkdir $1 #création du repertoire
 echo "Le Nom du projet est : $1 "
-cd $1
+cd $1 # on se deplace dans le repertoire
 touch main #création du fichier main
 touch LICENSE
 touch Makefile
 echo 0
-if [ $2 == c ] 
+if [ $2 == c ]  #on test quel langage l'utilisateur compte utilisé pour son projet
 then mv main main.$2
 cp ~/mysialhan/initdev/sources/main.c main.$2
-elif [ $2 == ccp ] || [ $2 == c++ ]
+echo $$?
+elif [ $2 == cpp ] || [ $2 == c++ ]
 then mv main main.$2
 cp ~/mysialhan/initdev/sources/main.cpp main.$2
-echo 0
+echo $?
 elif [ $2 == py ]
 then mv main main.$2
 cp ~/mysialhan/initdev/sources/main.py main.$2
-echo 0
+echo $?
 elif [ $2 == latex ]
 then mv main main.$2
 cp ~/mysialhan/initdev/sources/latexMin.tex main.$2
-echo 0
+echo $?
 elif [ $2 == beamer ]
 then mv main main .$2
 cp ~/mysialhan/initdev/sources/beamer.tex main.$2
-echo 0
-else echo "Uknown arguments, please check the help : initdev –help"
+echo $?
+else 
+echo "Uknown arguments, please check the help : initdev –help"
 fi
 fi
 fi
--- initdev/install.sh
+++ initdev/install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-mkdir /home/`whoami`/.initdev
-cp /home/`whoami`/mysialhan /home/`whoami`/.initdev
+if [  ! -e "/home/`whoami`/.initdev" ] # on test l'existance d'un répertoire déja nommé .initdev
+then
+mkdir /home/`whoami`/.initdev #on crée le repertoire caché initdev
+cp -r /home/`whoami`/mysialhan/* /home/`whoami`/.initdev #on copie les fichiers du dépot au nouveau répertoire
+else 
+echo $? #si le repertoire existe déja on affiche 1
+fi
+export PATH=$PATH:/home/usr/mysialhan/initdev #on exporte la variable PATH pour executer initdev comme commmande
+echo 'export PATH=$PATH:/home/usr/mysialhan' >> /home/usr/.bashrc #on copie dans bashrc pour que la commande devient pérmanente
